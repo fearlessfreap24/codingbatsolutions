9f9521c52390295faa91d3803b8dfa4eaf04093b
fearlessfreap24 <dylanaperez@yahoo.com>
2018-08-04 16:58:39 -0500

    solved make_bricks

--- Logic-2/make_bricks.py
+++ Logic-2/make_bricks.py
@@ -1,26 +1,36 @@
 def make_bricks(small, big, goal):
-    modgoal = goal // 5 # goal mod 5 to determine how many big bricks
-    # print("{} + {} = {}".format(big*5,small, (big*5)+small))
-    # print("modgoal = {}, big = {}".format(modgoal, big))
-    # print("modgoal <= big = {}".format(modgoal <= big))
-    # print("(5*big)+small >= goal = {}".format((5*big)+small >= goal))
-    return big <= modgoal and (5*big)+small >= goal
+
+    # watched part of coding bat video
+
+    if goal > (big * 5) + small:
+        return False
+
+    goalmod = goal % 5
+    if goalmod > small:
+        return False
+
+    return True
+
 
 
 
 if __name__ == "__main__":
-    if make_bricks(3, 1, 8):
+    print("3,2,8")
+    if make_bricks(3, 2, 8):
         print("true")
     else:
         print("false")
+    print("3,1,9")
     if make_bricks(3, 1, 9):
         print("true")
     else:
         print("false")
+    print("3,2,10")
     if make_bricks(3, 2, 10):
         print("true")
     else:
         print("false")
+    print("6,1,11")
     if make_bricks(6, 1, 11):
         print("true")
     else:
