ad7870d4bff7373b369275d84b534e8fcf27e71d
fearlessfreap24 <dylanaperez@yahoo.com>
2018-08-05 14:40:52 -0500

    solved make_chocolate

--- Logic-2/make_chocolate.py
+++ Logic-2/make_chocolate.py
@@ -1,13 +1,24 @@
 def make_chocolate(small, big, goal):
-    if goal > (big * 5) + small:
+
+    # not enough bricks
+    if big * 5 + small < goal:
         return -1
+
+    # not enough small bricks
     goalmod = goal % 5
     if goalmod > small:
         return -1
-    return goal - (big * 5)
+
+    # enough small bricks
+    if big < goal // 5:
+        return goal - (big * 5)
+    if big >= goal // 5:
+        return goal % 5
 
 
 if __name__ == "__main__":
     print(make_chocolate(4, 1, 9))  # 4
     print(make_chocolate(4, 1, 10))  # -1
     print(make_chocolate(4, 1, 7))  # 2
+    print(make_chocolate(6, 2, 7))  # 2
+    print(make_chocolate(7, 1, 12))  # 7
