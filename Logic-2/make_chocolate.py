ead48b8e4271768931e2c23e3ec0b6327ab0ba7f
dylanptcu <dylan.perez@tcu.edu>
2018-08-04 23:56:06 -0500

    format changes to make_chocolate

--- Logic-2/make_chocolate.py
+++ Logic-2/make_chocolate.py
@@ -6,7 +6,8 @@ def make_chocolate(small, big, goal):
         return -1
     return goal - (big * 5)
 
+
 if __name__ == "__main__":
-    print(make_chocolate(4, 1, 9)) # 4
-    print(make_chocolate(4, 1, 10)) # -1
-    print(make_chocolate(4, 1, 7)) # 2
+    print(make_chocolate(4, 1, 9))  # 4
+    print(make_chocolate(4, 1, 10))  # -1
+    print(make_chocolate(4, 1, 7))  # 2
