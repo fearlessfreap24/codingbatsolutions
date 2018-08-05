595a57c5076bbfdb0fb3b4e29fd1c72ea74596c2
fearlessfreap24 <dylanaperez@yahoo.com>
2018-08-04 22:11:22 -0500

    solved no_teen_sum

--- Logic-2/no_teen_sum.py
+++ Logic-2/no_teen_sum.py
@@ -1,15 +1,28 @@
 def no_teen_sum(a, b, c):
-    if a <= 19 and a >= 13:
+    sum = 0
+    if a in range(13,20):
         if a == 15 or a == 16:
-            fix_teen(a)
-    if b <= 19 and b >= 13:
+            sum += a
+        else:
+            sum += 0
+    else:
+        sum += a
+    if b in range(13,20):
         if b == 15 or b == 16:
-            fix_teen(b)
-    if c <= 19 and c >= 13:
+            sum += b
+        else:
+            sum += 0
+    else:
+        sum += b
+    if c in range(13,20):
         if c == 15 or c == 16:
-            fix_teen (c)
+            sum += c
+        else:
+            sum += 0
+    else:
+        sum += c
+    return sum
 
-def fix_teen(n):
 
 
 if __name__ == "__main__":
