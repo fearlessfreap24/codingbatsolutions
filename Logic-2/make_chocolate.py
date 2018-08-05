b8a92f29fe32d2093865766bf1e21fc6909989da
fearlessfreap24 <dylanaperez@yahoo.com>
2018-08-04 23:43:26 -0500

    more working on make_chocolate

--- Logic-2/make_chocolate.py
+++ Logic-2/make_chocolate.py
@@ -1,8 +1,10 @@
 def make_chocolate(small, big, goal):
-    if (big * 5) + small >= goal:
-        return goal % 5
-    else:
+    if goal > (big * 5) + small:
         return -1
+    goalmod = goal % 5
+    if goalmod > small:
+        return -1
+    return goal - (big * 5)
 
 if __name__ == "__main__":
     print(make_chocolate(4, 1, 9)) # 4
