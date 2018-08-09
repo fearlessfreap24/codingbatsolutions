8cd6cef4bc8d3c9ac778d7114420763d49588bfe
fearlessfreap24 <dylanaperez@yahoo.com>
2018-08-08 22:02:51 -0500

    completed sum67

--- List-2/sum67.py
+++ List-2/sum67.py
@@ -2,18 +2,30 @@ def sum67(nums):
     if 6 not in nums:
         return sum(nums)
     else:
-        sixes = nums.count(6)
-        sevs = nums.count(7)
-        six = nums.index(6)
-        sev = nums.index(7)
-        newnums = []
-        for i in range(sixes):
-            newnums += nums[:six]
-            nums = nums[six + 1:sev]
-        return sum(newnums)
+        ret = 0
+        flag = False
+        for num in nums:
+            if num == 6:
+                flag = True
+                continue
+            elif num == 7 and flag:
+                flag = False
+                continue
+
+            if not flag:
+                ret += num
+
+        return ret
+
+# I had to look this one up. it is a slight variation of what I found.
+# I initially have it checking if 6 exists in the list and if it doesn't return the sum of the list
+# I started with a loop and got the initial index of the first 6 and 7.
+# I had it working it there was only one 6 or 7. There lies the problem. The 4th print statement
+# has 2 6's and 7's.
 
 
 if __name__ == "__main__":
     print(sum67([1, 2, 2]))  # 5
     print(sum67([1, 2, 2, 6, 99, 99, 7]))  # 5
     print(sum67([1, 1, 6, 7, 2]))  # 4
+    print(sum67([1, 6, 2, 2, 7, 1, 6, 99, 99, 7]))  # 2
