1e5f1b6d197cf679aecba6316a69497f5a462e62
fearlessfreap24 <dylanaperez@yahoo.com>
2018-08-18 06:33:14 -0500

    created without_end

---
+++ String-1/without_end.py
@@ -0,0 +1,8 @@
+def without_end(str):
+    return str[1:-1]
+
+
+if __name__ == "__main__":
+    print(without_end('Hello'))  # 'ell'
+    print(without_end('java'))  # 'av'
+    print(without_end('coding'))  # 'odin'
